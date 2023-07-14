46808ff9d77e71212cd82e77dbd835884f26029e
Tarun vaidyula <126050619+Tarunvaidyula@users.noreply.github.com>
2023-07-14 09:49:07 +0530

    new commit 2

---
+++ littlelemon/tests/test-views.py
@@ -0,0 +1,8 @@
+from django.test import testcases
+
+class MenuViewTest(Testcase):
+    def test_get_all(self):
+        self.assertEqual()
+        
+        
+        
--- restaurant/models.py
+++ restaurant/models.py
@@ -18,8 +18,10 @@ class Menu(models.Model):
    price = models.IntegerField(null=False) 
    menu_item_description = models.TextField(max_length=1000, default='') 
 
-   def __str__(self):
+def __str__(self):
       return self.name
+def __str__(self):
+    return f'{self.title} : {str(self.price)}'
   
 class MenuItem(models.Model):
     title=models.CharField(max_length=255, db_index=True)    
